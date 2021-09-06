bef15b3aa11791eedc609822112bba61048a342e
sebi210dev <59232422+Sebight@users.noreply.github.com>
2021-09-06 21:06:00 +0200

    Collisions not working

---
+++ Kart-Racer/Assets/Scripts/Race/CollisionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    public BoxCollider col;
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("ya yeet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Kart-Racer/Assets/Scripts/Race/RaceHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RaceHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject start;
+    public List<GameObject> checkpoints = new List<GameObject>();
+    public int currentCheckpoint = 1;
+    
+    public void NextCheckpoint()
+    {
+        currentCheckpoint++;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("check");
+        if (other.gameObject.name == checkpoints[currentCheckpoint].name)
+        {
+            NextCheckpoint();
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("yoo");
+    }
+}
