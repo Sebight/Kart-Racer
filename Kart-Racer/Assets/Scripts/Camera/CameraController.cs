20b76d2824ff04bed64fe52bac8d9001f4a3a9bf
Sebight <59232422+Sebight@users.noreply.github.com>
2021-12-19 16:29:00 +0100

    Revert "Delete Kart-Racer directory"
    This reverts commit fb4eeee0a61372dce149412567e481d738ea1783.

---
+++ Kart-Racer/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform car;
+    public float smoothing;
+    public float rotationSmoothing;
+
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, car.position - transform.forward*10 - new Vector3(0, -5, 0), smoothing);
+        transform.rotation = Quaternion.Slerp(transform.rotation, car.rotation, rotationSmoothing);
+        transform.rotation = Quaternion.Euler(new Vector3(5, transform.rotation.eulerAngles.y, 0));
+    }
+}
---
+++ Kart-Racer/Assets/Scripts/Race/CollisionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    public BoxCollider col;
+
+    public RaceHandler raceHandler;
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        raceHandler.Collision(other.gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
